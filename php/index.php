56409f670cc9889e25b7466588b192dcb7cf0631
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-11 09:27:33 +0900

    practice

--- php/index.php
+++ php/index.php
@@ -4,10 +4,22 @@ echo(124);
 echo('<br>');
 echo 'test';
 
-$test = 123;
-// $test = 456;
-echo $test;
+$test1 = 123;
+$test2 = 456;
+
+echo $test1 . $test2;
+echo('<br>');
 
 $run = 'dash';
 echo $run;
+echo('<br>');
+
+const max = 10;
+echo max;
+
+$array = ['あああ',2,3];
+
+echo '<pre>';
+var_dump($array);
+      '</pre>';
 ?>
