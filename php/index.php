e6eb935a65397ee20842e1313d2970eb4fd5b053
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-11 14:06:18 +0900

    practice

--- php/index.php
+++ php/index.php
@@ -1,30 +1,18 @@
 <?php
-// 数字だよ
-echo(124);
-echo('<br>');
-echo 'test';
-
-$test1 = 123;
-$test2 = 456;
-
-echo $test1 . $test2;
-echo('<br>');
-
-$run = 'dash';
-echo $run;
-echo('<br>');
-
-const max = 10;
-echo max;
-
-$array2 = [
-  ['赤','青','黄'],
-  ['緑','紫','黒']
+$members_2 = [
+    'ホンダ' => [
+      'height' => 170,
+      'hobby' => 'サッカー'
+    ],
+    '香川' => [
+      'height' => 175,
+      'hobby' => 'ドリブル'
+    ]
 ];
 
-echo '<pre>';
-var_dump($array2);
-      '</pre>';
-
-echo $array2[1][1]
+foreach ($members_2 as $member_1){
+  foreach($member_1 as $member){
+    echo $member;
+  }
+}
 ?>
