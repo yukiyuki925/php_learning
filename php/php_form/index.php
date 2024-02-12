4e27d32c43a1a3ae123ee1f435cabe416ca76f5c
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-12 09:59:46 +0900

    easy form

--- php/php_form/index.php
+++ php/php_form/index.php
@@ -35,6 +35,7 @@ if(!empty($_POST['btn_submit'])){
     メールアドレス
     <?php echo $_POST['email'];?>
     <br>
+    <input type="submit" name="back" value="戻る">
     <input type="submit" name="btn_submit" value="送信する">
     <input type="hidden" name="your_name" value="<?php echo $_POST['your_name'];?>">
     <input type="hidden" name="email" value="<?php echo $_POST['email'];?>">
@@ -48,10 +49,10 @@ if(!empty($_POST['btn_submit'])){
   <?php if ($pageFlag === 0) : ?>
   <form action="index.php" method="post">
     氏名
-    <input type="text" name="your_name">
+    <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])) echo $_POST['your_name'];?>">
     <br>
     メールアドレス
-    <input type="email" name="email">
+    <input type="email" name="email" value="<?php if(!empty($_POST['email'])) echo $_POST['email'];?>">
     <br>
     <input type="submit" name="btn_confirm" value="確認する">
   </form>
