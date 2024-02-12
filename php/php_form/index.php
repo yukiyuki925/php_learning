8e064b6266f3a34efc5698f8aef8c0ac300a8392
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-13 08:22:41 +0900

    validation

--- php/php_form/index.php
+++ php/php_form/index.php
@@ -1,8 +1,8 @@
 <?php
 // スーパーグローバル変数
-if(!empty($_SESSION)){
+if(!empty($_POST)){
   echo '<pre>';
-  var_dump($_SESSION);
+  var_dump($_POST);
   echo '</pre>';
 }
 session_start();
@@ -75,6 +75,31 @@ if(!empty($_POST['btn_submit'])){
     <br>
     メールアドレス
     <input type="email" name="email" value="<?php if(!empty($_POST['email'])) echo h($_POST['email']);?>">
+    <br>
+    ホームページ
+    <input type="url" name="url" value="<?php if(!empty($_POST['url'])) echo h($_POST['email']);?>">
+    <br>
+    性別
+    <input type="radio" name="gender" value="0">男性
+    <input type="radio" name="gender" value="1">女性
+    <br>
+    年齢
+    <select name="age">
+      <option value="">選択してください</option>
+      <option value="1">〜19歳</option>
+      <option value="2">20歳〜29歳</option>
+      <option value="3">30歳〜39歳</option>
+      <option value="4">40歳〜49歳</option>
+      <option value="5">50歳〜59歳</option>
+      <option value="6">60歳〜69歳</option>
+    </select>
+    <br>
+    お問い合わせ内容
+    <textarea name="contact">
+      <?php if(!empty($_POST['contact'])) echo h($_POST['contact']);?>
+    </textarea>
+    <br>
+    <input type="checkbox" name="caution" value="1">注意事項にチェックする
     <br>
     <input type="submit" name="btn_confirm" value="確認する">
     <input type="hidden" name="csrf" value="<?php echo $token; ?>">
