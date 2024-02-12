0dfca61c1c13b761c63e4035690504d89262cbe5
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-12 09:42:50 +0900

    form push

--- php/php_form/index.php
+++ php/php_form/index.php
@@ -1,9 +1,19 @@
 <?php
-if(!empty($_GET)){
-  var_dump($_GET);
+if(!empty($_POST)){
+  echo '<pre>';
+  var_dump($_POST);
+  echo '</pre>';
 }
 // スーパーグローバル変数
 
+$pageFlag = 0;
+
+if(!empty($_POST['btn_confirm'])){
+  $pageFlag = 1;
+}
+if(!empty($_POST['btn_submit'])){
+  $pageFlag = 2;
+}
 ?>
 
 <!DOCTYPE html>
@@ -16,15 +26,36 @@ if(!empty($_GET)){
 </head>
 
 <body>
-  <form action="index.php" method="get">
+
+  <?php if ($pageFlag === 1) : ?>
+  <form action="index.php" method="post">
+    氏名
+    <?php echo $_POST['your_name'];?>
+    <br>
+    メールアドレス
+    <?php echo $_POST['email'];?>
+    <br>
+    <input type="submit" name="btn_submit" value="送信する">
+    <input type="hidden" name="your_name" value="<?php echo $_POST['your_name'];?>">
+    <input type="hidden" name="email" value="<?php echo $_POST['email'];?>">
+  </form>
+  <?php endif; ?>
+
+  <?php if ($pageFlag === 2) : ?>
+  送信が完了しました
+  <?php endif; ?>
+
+  <?php if ($pageFlag === 0) : ?>
+  <form action="index.php" method="post">
     氏名
     <input type="text" name="your_name">
     <br>
-    <input type="checkbox" name="sports[]" value="野球">野球
-    <input type="checkbox" name="sports[]" value="サッカー">サッカー
-    <input type="checkbox" name="sports[]" value="バスケ">バスケ
-    <input type="submit" value="送信">
+    メールアドレス
+    <input type="email" name="email">
+    <br>
+    <input type="submit" name="btn_confirm" value="確認する">
   </form>
+  <?php endif; ?>
 
 </body>
 
