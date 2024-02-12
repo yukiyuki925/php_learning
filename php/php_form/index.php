df2bb5f8fd171a9d9f8617f2502fec07b0d056b6
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-12 15:29:06 +0900

    form push

--- php/php_form/index.php
+++ php/php_form/index.php
@@ -1,10 +1,16 @@
 <?php
-if(!empty($_POST)){
-  echo '<pre>';
-  var_dump($_POST);
-  echo '</pre>';
-}
 // スーパーグローバル変数
+// if(!empty($_POST)){
+//   echo '<pre>';
+//   var_dump($_POST);
+//   echo '</pre>';
+// }
+header('X-FRAME-OPTIONS:DENY');
+
+function h($str)
+{
+  return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
+}
 
 $pageFlag = 0;
 
@@ -22,7 +28,7 @@ if(!empty($_POST['btn_submit'])){
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>PHP_form</title>
 </head>
 
 <body>
@@ -30,15 +36,15 @@ if(!empty($_POST['btn_submit'])){
   <?php if ($pageFlag === 1) : ?>
   <form action="index.php" method="post">
     氏名
-    <?php echo $_POST['your_name'];?>
+    <?php echo h($_POST['your_name']);?>
     <br>
     メールアドレス
-    <?php echo $_POST['email'];?>
+    <?php echo h($_POST['email']);?>
     <br>
     <input type="submit" name="back" value="戻る">
     <input type="submit" name="btn_submit" value="送信する">
-    <input type="hidden" name="your_name" value="<?php echo $_POST['your_name'];?>">
-    <input type="hidden" name="email" value="<?php echo $_POST['email'];?>">
+    <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']);?>">
+    <input type="hidden" name="email" value="<?php echo h($_POST['email']);?>">
   </form>
   <?php endif; ?>
 
@@ -49,10 +55,10 @@ if(!empty($_POST['btn_submit'])){
   <?php if ($pageFlag === 0) : ?>
   <form action="index.php" method="post">
     氏名
-    <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])) echo $_POST['your_name'];?>">
+    <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])) echo h($_POST['your_name']);?>">
     <br>
     メールアドレス
-    <input type="email" name="email" value="<?php if(!empty($_POST['email'])) echo $_POST['email'];?>">
+    <input type="email" name="email" value="<?php if(!empty($_POST['email'])) echo h($_POST['email']);?>">
     <br>
     <input type="submit" name="btn_confirm" value="確認する">
   </form>
