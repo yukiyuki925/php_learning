d833bef37ab248d722d8025719510fbdf17b4a5a
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-17 09:29:22 +0900

    basic practice

---
+++ php/lesson.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php echo "\"php\"を勉強中です"; ?>
+  <?php echo 123 + 2 * 5 / 3?>
+</body>
+
+</html>
