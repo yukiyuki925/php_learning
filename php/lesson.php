6f3325ba944679eb95e6697dba464f78af312f8c
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-17 10:21:49 +0900

    php basic practice

--- php/lesson.php
+++ php/lesson.php
@@ -8,8 +8,28 @@
 </head>
 
 <body>
-  <?php echo "\"php\"を勉強中です"; ?>
-  <?php echo 123 + 2 * 5 / 3?>
+  <?php echo "\"php\"を勉強中です";
+  echo '<br>'
+  ?>
+  <?php 
+  $sum = 100 + 1050 + 200;
+  echo $sum;
+  echo '<br>';
+  ?>
+  <p>合計金額は<?php echo $sum; ?>円です</p>
+  <p>税込金額は<?php echo $sum * 1.1; ?>円です</p>
+  <?php 
+  $today = new Datetime();
+  echo '現在は' . $today->format('G時 i分 s秒') . 'です';
+  echo '<br>'
+  ?>
+
+  <?php 
+  for ($i = 1; $i < 31; $i++) {
+    $unko =  $i / 2;
+    echo $unko . "<br>";
+  }
+  ?>
 </body>
 
 </html>
