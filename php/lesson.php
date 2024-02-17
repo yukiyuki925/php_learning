51e270b60b99fbb686e99f9678afb46d8664849c
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-17 14:32:16 +0900

    practice

--- php/lesson.php
+++ php/lesson.php
@@ -15,6 +15,39 @@
   $week = date('w');
   echo $week_name[$week];
   ?>
+  <?php
+  $fruits = [
+    'apple' => 'りんご',
+    'grape' => 'ぶどう',
+    'lemon' => 'レモン',
+    'tomato' => 'トマト',
+    'peach' => 'もも'
+  ];
+  ?>
+
+  <dl>
+    <?php foreach($fruits as $english => $japanese): ?>
+    <dt><?php echo $english; ?></dt>
+    <dd><?php echo $japanese; ?></dd>
+    <?php endforeach; ?>
+  </dl>
+
+  <?php 
+  date_default_timezone_set('Asia/Tokyo');
+  $time = date("G");
+  ?>
+  <?php if($time < 17): ?>
+  <p>営業時間外です</p>
+  <?php else: ?>
+  <p>ようこそ</p>
+  <?php endif ?>
+
+  <?php
+  $x = 0;
+  if(!$x){
+    echo '$x = 0';
+  }
+  ?>
 </body>
 
 </html>
