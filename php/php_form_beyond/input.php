7a3525095140a2f4f0b796cc8cfdcc27f90c4ab0
yukiyuki925 <yuki.work1999@gmail.com>
2024-03-01 18:38:21 +0900

    form 再学習

--- php/php_form_beyond/input.php
+++ php/php_form_beyond/input.php
@@ -1,6 +1,8 @@
 <?php
 if (!empty($_POST)) {
-  var_dump($_POST['btn_confirm']);
+  echo '<pre>';
+  var_dump($_POST);
+  echo '</pre>';
 }
 
 // 入力、確認、完了 input.php, confirm.php, thanks.php
@@ -39,6 +41,7 @@ if (!empty($_POST['btn_submit'])) {
     メールアドレス
     <?php echo $_POST['email']; ?>
     <br>
+    <input type="submit" name="back" value="戻る">
     <input type="submit" name="btn_submit" value="送信する">
     <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
     <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
@@ -56,10 +59,10 @@ if (!empty($_POST['btn_submit'])) {
   <?php if ($pageFlag === 0) :?>
   <form method="POST" action="input.php">
     氏名
-    <input type="text" name="your_name">
+    <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name'])) echo $_POST['your_name']; ?>">
     <br>
     メールアドレス
-    <input type="email" name="email">
+    <input type="email" name="email" value="<?php if(!empty($_POST['email'])) echo $_POST['email']; ?>">
     <br>
     <input type="submit" name="btn_confirm" value="確認する">
   </form>
